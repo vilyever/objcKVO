d190c5cf76499d126bd9627b01d447f2d2c8a7c3
vilyever <250450035@163.com>
2016-08-05 09:40:57 +0800

    Initial Commit

---
+++ objcKVO/objcKVO.h
@@ -0,0 +1,19 @@
+//
+//  objcKVO.h
+//  objcKVO
+//
+//  Created by FTET on 16/8/5.
+//  Copyright © 2016年 vilyever. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for objcKVO.
+FOUNDATION_EXPORT double objcKVOVersionNumber;
+
+//! Project version string for objcKVO.
+FOUNDATION_EXPORT const unsigned char objcKVOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <objcKVO/PublicHeader.h>
+
+
